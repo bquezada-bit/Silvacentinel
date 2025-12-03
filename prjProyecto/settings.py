d0e8a4182c8b6715f91ca0536d490315465ddd79
bquezada-bit <bquezada.bqc@gmail.com>
2025-12-03 18:11:12 -0300

    Arreglar rutas y estructura de static

--- prjProyecto/settings.py
+++ prjProyecto/settings.py
@@ -155,13 +155,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static' / 'app',
+    BASE_DIR / 'static',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 # ==============================================================================
 # REST FRAMEWORK
 # ==============================================================================
